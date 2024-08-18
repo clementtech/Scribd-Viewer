89632132018e9466d6d3c9f3ebc0c7a72a9bc717
Clement <119944151+clementtech@users.noreply.github.com>
2024-08-18 22:10:34 +0800

    Update scribd_bot.py
    Cosmetic improvement.
    
    Fix error shown when not pasting Scribd document link

--- scribd_bot.py
+++ scribd_bot.py
@@ -1,10 +1,15 @@
-url_input = str(input("Scribd URL: "))
-
 # Sample URL: https://www.scribd.com/document/441480158/User-Manual-for-LED-Digital-Clock
+while True:
+    try:
+        url_input = str(input("Scribd URL: "))
 
-document_id = url_input.split("/")[4]
+        document_id = url_input.split("/")[4]
 
-url_output = f"https://www.scribd.com/embeds/{document_id}/content?start_page=1&view_mode=scroll"
+    except IndexError:
+        print("Please enter a valid Scribd Document URL")
+
+    else:
+        url_output = f"Enjoy: https://www.scribd.com/embeds/{document_id}/content?start_page=1&view_mode=scroll"
+        break
 
 print(url_output)
-
