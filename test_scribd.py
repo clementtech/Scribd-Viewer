dbfd50bb79b37213f10fd51dfd575d5bc128dd58
Clement Chong <119944151+clementtech@users.noreply.github.com>
2024-09-06 00:02:19 +0800

    Fix bugs

--- test_scribd.py
+++ test_scribd.py
@@ -1,6 +1,6 @@
 # Code to test scribd_bot.py
 
-import scribd_bot
+from scribd_bot import bypass
 
 def test_link():
     assert bypass("https://www.scribd.com/document/441480158/User-Manual-for-LED-Digital-Clock") == "Enjoy: https://www.scribd.com/embeds/441480158/content?start_page=1&view_mode=scroll"
