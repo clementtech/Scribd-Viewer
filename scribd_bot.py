99b45094a5b1f7cf21fbd6c762e5a26eb7723381
Clement Chong <119944151+clementtech@users.noreply.github.com>
2024-09-05 15:44:16 +0000

    adding testing function to code

--- scribd_bot.py
+++ scribd_bot.py
@@ -1,32 +1,28 @@
 # Sample URL: https://www.scribd.com/document/441480158/User-Manual-for-LED-Digital-Clock
 
-# Creates a loop to repeat asking user if input is not a valid document link
-while True:
+def main():
+    # Creates a loop to repeat asking user if input is not a valid document link
+    while True:
 
-    # If it's a valid document link
-    try:
+        # If it's a valid document link
+        try:
 
-        # Receive input from the user in string format
-        url_input = str(input("Scribd URL: "))
+            # Receive input from the user in string format
+            print(bypass((str(input("Scribd URL: "))).split("/")[4]))
 
-        # Process the document link and strip out the document id
-        document_id = url_input.split("/")[4]
+        # If user input not a valid Scribd document id
+        except IndexError:
 
-    # If user input not a valid Scribd document id
-    except IndexError:
+            # Tells the user to input a valid document link
+            print("Please enter a valid Scribd Document URL")
 
-        # Tells the user to input a valid document link
-        print("Please enter a valid Scribd Document URL")
+        except EOFError:
+            break
+        
 
-    # If the user input the valid document link
-    else:
-
-        # New link where paywall is removed
-        url_output = f"Enjoy: https://www.scribd.com/embeds/{document_id}/content?start_page=1&view_mode=scroll"
-
-        # Stop the loop
-        break
+def bypass(id)
+    return f"Enjoy: https://www.scribd.com/embeds/{id}/content?start_page=1&view_mode=scroll"
 
 
-# Print out the new link where paywall is removed
-print(url_output)
+if __name__ == "__main__":
+    main()
