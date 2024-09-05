7ce543b2d6e082d419d5df7d09f80e107bee2c7a
Clement Chong <119944151+clementtech@users.noreply.github.com>
2024-09-05 23:51:48 +0800

    Update test_scribd.py
    Assertion Error fix

--- test_scribd.py
+++ test_scribd.py
@@ -3,4 +3,4 @@
 import scribd_bot
 
 def test_link():
-    assert bypass("https://www.scribd.com/document/441480158/User-Manual-for-LED-Digital-Clock") == "https://www.scribd.com/embeds/441480158/content?start_page=1&view_mode=scroll"
+    assert bypass("https://www.scribd.com/document/441480158/User-Manual-for-LED-Digital-Clock") == "Enjoy: https://www.scribd.com/embeds/441480158/content?start_page=1&view_mode=scroll"
