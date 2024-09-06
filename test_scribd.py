4a888beb0d77eb1cd70227f52397961eb9e57210
Clement Chong <119944151+clementtech@users.noreply.github.com>
2024-09-06 08:35:53 +0800

    Update test_scribd.py

--- test_scribd.py
+++ test_scribd.py
@@ -1,6 +1,8 @@
 # Code to test scribd_bot.py
 
+# Import the bypass feature from the scribd_bot program
 from scribd_bot import bypass
 
+# Create a test to checks if the link sucessfully converted into a non paylink document
 def test_link():
-    assert bypass("https://www.scribd.com/document/441480158/User-Manual-for-LED-Digital-Clock") == "Enjoy: https://www.scribd.com/embeds/441480158/content?start_page=1&view_mode=scroll"
+    assert bypass("https://www.scribd.com/document/441480158/User-Manual-for-LED-Digital-Clock") == "https://www.scribd.com/embeds/441480158/content?start_page=1&view_mode=scroll"
