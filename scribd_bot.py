b4d0f980277d80d6e818ea1f0e12142bca8bb211
Clement Chong <119944151+clementtech@users.noreply.github.com>
2024-09-06 08:30:10 +0800

    Update scribd_bot.py
    Add documentation

--- scribd_bot.py
+++ scribd_bot.py
@@ -1,14 +1,23 @@
 # Sample URL: https://www.scribd.com/document/441480158/User-Manual-for-LED-Digital-Clock
 
+# Define main function to use self defined function for code testing
 def main():
+
     # Creates a loop to repeat asking user if input is not a valid document link
     while True:
 
         # If it's a valid document link
         try:
 
-            # Receive input from the user in string format
-            print(bypass((str(input("Scribd URL: "))).split("/")[4]))
+            # 1. Receive link from the user input
+            # 2. Convert it to string
+            # 3. Bypass function to convert URL into bypass paywall URL
+            # 4. Print the new link out with the "Enjoy: " word infront
+            print("Enjoy:" , bypass(str(input("Scribd URL: "))))
+
+            # If everything is done, which means no error/valid url
+            # Stops the loop and the program will stop functioning
+            break
 
         # If user input not a valid Scribd document id
         except IndexError:
@@ -16,13 +25,19 @@ def main():
             # Tells the user to input a valid document link
             print("Please enter a valid Scribd Document URL")
 
+        # If control-d is inputted, stop the function
         except EOFError:
             break
 
-
+# Define new bypass function to remove paywall
 def bypass(id):
-    return f"Enjoy: https://www.scribd.com/embeds/{id}/content?start_page=1&view_mode=scroll"
 
+    # Receive the document id number by spliting everything and only select the number
+    id = id.split("/")[4]
 
+    # Return the new URL with paywall bypassed
+    return f"https://www.scribd.com/embeds/{id}/content?start_page=1&view_mode=scroll"
+
+# Only run this function if it is intended to do so
 if __name__ == "__main__":
     main()
